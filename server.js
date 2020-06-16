e3917238c724f0df5ef818583ec772d69c117188
ShibbyBro <tanneracord@gmail.com>
2020-06-16 09:50:15 -0700

    Changing database

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-flat-20634',
     user : 'tantan',
     password : '',
     database : 'smart-brain'
